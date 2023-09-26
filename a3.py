8868c1284988a5eb442eab16509da8fe19be58c0
Ewan <ejmcphilliam@cps.edu>
2023-09-26 11:28:41 -0500

    modified:   a3.py

--- a3.py
+++ a3.py
@@ -54,7 +54,13 @@ def title_by_year(matches: List[str]) -> List[str]:
     Returns:
         a list of movie titles made in the passed in year
     """
-    pass
+    for year in matches:
+        to_return = []
+        for movie in movie_db:
+            if (get_year(movie) == int(year)):
+                to_return.append(get_title(movie))
+    return to_return
+
 
 
 def title_by_year_range(matches: List[str]) -> List[str]:
@@ -226,14 +232,14 @@ def query_loop() -> None:
 
 if __name__ == "__main__":
     assert isinstance(title_by_year(["1974"]), list), "title_by_year not returning a list"
-    assert isinstance(title_by_year_range(["1970", "1972"]), list), "title_by_year_range not returning a list"
-    assert isinstance(title_before_year(["1950"]), list), "title_before_year not returning a list"
-    assert isinstance(title_after_year(["1990"]), list), "title_after_year not returning a list"
-    assert isinstance(director_by_title(["jaws"]), list), "director_by_title not returning a list"
-    assert isinstance(title_by_director(["steven spielberg"]), list), "title_by_director not returning a list"
-    assert isinstance(actors_by_title(["jaws"]), list), "actors_by_title not returning a list"
-    assert isinstance(year_by_title(["jaws"]), list), "year_by_title not returning a list"
-    assert isinstance(title_by_actor(["orson welles"]), list), "title_by_actor not returning a list"
+    # assert isinstance(title_by_year_range(["1970", "1972"]), list), "title_by_year_range not returning a list"
+    # assert isinstance(title_before_year(["1950"]), list), "title_before_year not returning a list"
+    # assert isinstance(title_after_year(["1990"]), list), "title_after_year not returning a list"
+    # assert isinstance(director_by_title(["jaws"]), list), "director_by_title not returning a list"
+    # assert isinstance(title_by_director(["steven spielberg"]), list), "title_by_director not returning a list"
+    # assert isinstance(actors_by_title(["jaws"]), list), "actors_by_title not returning a list"
+    # assert isinstance(year_by_title(["jaws"]), list), "year_by_title not returning a list"
+    # assert isinstance(title_by_actor(["orson welles"]), list), "title_by_actor not returning a list"
     
     assert sorted(title_by_year(["1974"])) == sorted(
         ["amarcord", "chinatown"]
@@ -262,7 +268,7 @@ if __name__ == "__main__":
             "murray hamilton",
         ]
     ), "failed actors_by_title test"
-    assert sorted(actors_by_title(["movie not in database"])) == []), "failed actors_by_title not in database test"
+    assert sorted(actors_by_title(["movie not in database"])) == [], "failed actors_by_title not in database test"
     assert sorted(year_by_title(["jaws"])) == sorted(
         [1975]
     ), "failed year_by_title test"
